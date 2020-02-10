d90c5c412184ea18c98a3be28c0a9aad8f13416f
Alexa Lewis <alexalewisdevelops@gmail.com>
2020-02-10 17:21:24 -0500

    Fixed variable names

--- Program.cs
+++ Program.cs
@@ -27,16 +27,16 @@ namespace PlayingWithVariables
       var operand1 = Console.ReadLine();
       Console.WriteLine("Please give me another number");
       var operand2 = Console.ReadLine();
-      var userSum = double.Parse(operand1) + double.Parse(operand2);
-      var userDiff = double.Parse(operand1) - double.Parse(operand2);
-      var userQuotient = double.Parse(operand1) / double.Parse(operand2);
-      var userProduct = double.Parse(operand1) * double.Parse(operand2);
+      var sum = double.Parse(operand1) + double.Parse(operand2);
+      var difference = double.Parse(operand1) - double.Parse(operand2);
+      var quotient = double.Parse(operand1) / double.Parse(operand2);
+      var product = double.Parse(operand1) * double.Parse(operand2);
       var remainder = double.Parse(operand1) % double.Parse(operand2);
 
-      Console.WriteLine("The sum of your two numbers is " + userSum);
-      Console.WriteLine("The difference of your numbers is " + userDiff);
-      Console.WriteLine("The quotient of your numbers is " + userQuotient);
-      Console.WriteLine("The product of your numbers is " + userProduct);
+      Console.WriteLine("The sum of your two numbers is " + sum);
+      Console.WriteLine("The difference of your numbers is " + difference);
+      Console.WriteLine("The quotient of your numbers is " + quotient);
+      Console.WriteLine("The product of your numbers is " + product);
       Console.WriteLine("The remainder of your numbers is " + remainder);
 
     }
